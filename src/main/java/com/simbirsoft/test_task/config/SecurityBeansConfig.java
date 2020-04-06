e4f992a0eb2ee4d88bcf666f3818a43020b30c8f
mikhail.fomin <mikhail.fomin@simbirsoft.com>
2020-04-07 00:48:32 +0400

    explaration added

--- src/main/java/com/simbirsoft/test_task/config/SecurityBeansConfig.java
+++ src/main/java/com/simbirsoft/test_task/config/SecurityBeansConfig.java
@@ -3,7 +3,6 @@ package com.simbirsoft.test_task.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
@@ -12,7 +11,13 @@ public class SecurityBeansConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         Argon2PasswordEncoder encoder
-                = new Argon2PasswordEncoder(16, 32, 1, 4096, 3);
+                = new Argon2PasswordEncoder(
+                    /*saltLength*/ 16,
+                    /*hashLength*/ 32,
+                    /*parallelism*/1,
+                    /*memory*/ 4096,
+                    /*iterations*/ 3
+        );
         return encoder;
     }
 
